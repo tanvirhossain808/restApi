44594e32d516d0595f8b4abf01b4979e020a2bc0
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-04-24 00:23:28 +0600

    add sort

--- controlllers/products.js
+++ controlllers/products.js
@@ -2,8 +2,21 @@ const product = require("../Models/product")
 
 const getAllProducts = async (req, res) => {
 
-    const myData = await product.find()
+    // const myData = await product.find()
+    const queryObject = {}
+    const { company, name, sort } = req.query
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" }
+    }
+    let apiData = product.find(queryObject)
+    if (sort) {
+        let sortFix = sort.replace(",", " ")
+        // queryObject.sort = sortFix
+        console.log('heyd');
+        apiData = apiData.sort(sortFix)
+    }
     // res.status(200).json({ msg: "I am getAllProducts" })
+    const myData = await apiData
     res.status(200).json({ myData })
 }
 
@@ -14,11 +27,13 @@ const getAllProductsTesting = async (req, res) => {
     if (company) {
         queryObject.company = company
     }
-    if (name) {
-        queryObject.name = { $regex: name, $options: "i" }
-    }
+    // if (name) {
+    //     queryObject.name = { $regex: name, $options: "i" }
+    // }
+
     // const myData = await product.find(req.query)
-    const myData = await product.find(queryObject)
+    // const myData = await product.find(queryObject)
+    const myData = await product.find(req.query).sort("company")
     console.log(req.query);
     res.status(200).json({ myData })
 }
